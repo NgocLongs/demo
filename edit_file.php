7e1bf8d9ffb79f058af9c3ca0b2b72554803dd74
Ngoc Long <46261653+NgocLongs@users.noreply.github.com>
2019-09-26 08:13:37 +0700

    init project

---
+++ edit_file.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Demo Page</title>
+</head>
+<body>
+	<div class="navbar">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="#">Title</a>
+			<ul class="nav navbar-nav">
+				<li class="active">
+					<a href="#">Home</a>
+				</li>
+				<li>
+					<a href="#">Link</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</body>
+</html>
